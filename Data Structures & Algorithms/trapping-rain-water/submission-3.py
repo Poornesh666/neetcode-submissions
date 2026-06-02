4e28eddc61c224583512ce28083dd89e6347f296
Poornesh666 <140740020+Poornesh666@users.noreply.github.com>
2026-06-02 10:42:50 +0530

    Add: trapping-rain-water - submission-3

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-3.py
@@ -0,0 +1,26 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        n = len(height)
+        res = 0
+
+        if n == 0:
+            return 0
+
+        prefix_arr = [0]*n
+        suffix_arr = [0]*n
+        maxi1 = height[0]
+        maxi2 = height[n-1]
+
+        for i in range(n):
+            maxi1 = max(maxi1, height[i])
+            suffix_arr[i] = maxi1
+
+        for i in range(n-1, -1, -1):
+            maxi2 = max(maxi2, height[i])
+            suffix_arr[i] = min(suffix_arr[i], maxi2)
+            res += suffix_arr[i] - height[i]
+
+        # for i in range(n):
+        #     res += min(suffix_arr[i], prefix_arr[i]) - height[i]
+
+        return res
