a55b337f3b5f79b08f83ef488229a83832ab10da
Poornesh666 <140740020+Poornesh666@users.noreply.github.com>
2026-07-11 13:05:56 +0530

    Add: jump-game-ii - submission-1

---
+++ Data Structures & Algorithms/jump-game-ii/submission-1.py
@@ -0,0 +1,21 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        memo = {}
+        def dfs(i):
+            nonlocal memo
+            if i in memo:
+                return memo[i]
+
+            if i == len(nums) - 1:
+                return 0
+
+            ans = float("+inf")
+
+            end = min(len(nums)-1, i+nums[i])
+            for j in range(i+1,end+1):
+                ans = min(ans, 1 + dfs(j))
+
+            memo[i] = ans
+            return ans
+
+        return dfs(0)
