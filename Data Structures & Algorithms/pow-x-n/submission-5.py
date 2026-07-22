73c062f480854d14d4352c7dd1b6cc7483ddaa4d
Poornesh <140740020+Poornesh666@users.noreply.github.com>
2026-07-22 09:54:46 +0530

    Add: pow-x-n - submission-5

---
+++ Data Structures & Algorithms/pow-x-n/submission-5.py
@@ -0,0 +1,16 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if n < 0:
+            x = 1/x
+            n = -n
+        
+        ans = 1
+        while n:
+            if n&1:
+                ans *= x
+            
+            
+            n >>= 1
+            x *= x
+
+        return ans
